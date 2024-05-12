c42c7039e003af324d55716dccc8515b50d0b52d
Naminar <buravko.va@phystech.edu>
2024-05-12 12:59:47 +0300

    Added taging for solver.h

--- lab2/inc/solver.h
+++ lab2/inc/solver.h
@@ -1,7 +1,12 @@
 
+#ifndef SOLVER_H
+#define SOLVER_H
+
 #include "queue.h"
 
 RangeBox* get_box(Queue* qu);
 void push_box(Queue* qu, double x_min, double x_max);
 Queue* init_queue();
 void free_queue(Queue* qu);
+
+#endif
